1d868578dce7bdc96c65c3b5fec15f345deb3cce
Michflorez <167869106+Michflorez@users.noreply.github.com>
2024-09-24 19:47:44 +0000

    [añadir diagram]

--- src/models.py
+++ src/models.py
@@ -1,29 +1,27 @@
-import os
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.orm import relationship, declarative_base
-from sqlalchemy import create_engine
-from eralchemy2 import render_er
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-Base = declarative_base()
+
 
 class User(db.Model):
     __tablename__ = 'user'
     userId = db.Column(db.Integer, primary_key=True)
     userName = db.Column(db.String(25), nullable=False)
     email = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(80), unique=False, nullable=False)
+    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+
     user_favorites = db.relationship('Favorites', backref='user', lazy=True)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
     def serialize(self):
         return {
-            'userId': self.userId,
-            'userName': self.userName,
-            'email': self.email
+            "id": self.userId,
+            "email": self.email,
+            "username": self.userName,
         }
-
-
-
-
 class Character(db.Model):
     __tablename__ = 'character'
     characterId = db.Column(db.Integer, primary_key=True)
@@ -33,21 +31,24 @@ class Character(db.Model):
     height = db.Column(db.Integer)
     weight = db.Column(db.Integer)
     age = db.Column(db.Integer)
+
     character_favorites = db.relationship('Favorites', backref='character', lazy=True)
-    
+
+    def __repr__(self):
+        return '<Character %r>' % self.name
+
     def serialize(self):
         return {
-            'characterId': self.characterId,
-            'name': self.name,
-            'species': self.species,
-            'gender': self.gender,
-            'height': self.height,
-            'weight': self.weight,
-            'age': self.age
+            "id": self.characterId,
+            "species": self.species,
+            "name": self.name,
+            "gender": self.gender,
+            "name": self.gender,
+            "height": self.height,
+            "weight": self.weight,
+            "age": self.age,
         }
-
     
-
 
 class Planet(db.Model):
     __tablename__ = 'planet'
@@ -59,9 +60,22 @@ class Planet(db.Model):
     orbitalPeriod = db.Column(db.Integer)
     climate = db.Column(db.String(25))
 
-
     planet_favorites = db.relationship('Favorites', backref='planet', lazy=True)
 
+    def __repr__(self):
+        return '<Planet %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.planetId,
+            "name": self.name,
+            "population": self.population,
+            "gender": self.gender,
+            "diameter": self.diameter,
+            "rotationPeriod": self.rotationPeriod,
+            "orbitalPeriod": self.orbitalPeriod,
+            "climate": self.climate,
+        }
 
 class Vehicle(db.Model):
     __tablename__ = 'vehicle'
@@ -71,19 +85,22 @@ class Vehicle(db.Model):
     model = db.Column(db.String(25))
     passengers = db.Column(db.Integer)
     loadCapacity = db.Column(db.Integer)
- 
-def serialize(self):
-        return {
-            'vehicleId': self.vehicleId,
-            'name': self.name,
-            'vehicleClass': self.vehicleClass,
-            'model': self.model,
-            'passengers': self.passengers,
-            'loadCapacity': self.loadCapacity
-        }
-    
-vehicle_favorites = db.relationship('Favorites', backref='vehicle', lazy=True)
 
+    vehicle_favorites = db.relationship('Favorites', backref='vehicle', lazy=True)       
+
+    def __repr__(self):
+        return '<Vehicle %r>' % self.vehicle
+
+    def serialize(self):
+        return {
+            "id": self.vehicleId,
+            "name": self.name,
+            "vehicleClass": self.vehicleClass,
+            "model": self.model,
+            "passenger": self.passengers,
+            "LoadCapacity": self.loadCapacity,
+        
+        }
 class Favorites(db.Model):
     __tablename__ = 'favorites'
     id = db.Column(db.Integer, primary_key=True)
@@ -92,17 +109,13 @@ class Favorites(db.Model):
     planetId = db.Column(db.Integer, db.ForeignKey('planet.planetId'), nullable=False)
     vehicleId = db.Column(db.Integer, db.ForeignKey('vehicle.vehicleId'), nullable=False)
 
-    def serialize(self):
-        return {
-            'id': self.id,
-            'userId': self.userId,
-            'characterId': self.characterId,
-            'planetId': self.planetId,
-            'vehicleId': self.vehicleId
-        }
 
     def to_dict(self):
-        return {}
+        return {
+            "id":self.id,
+            "userID":self.userId,
+            "characterId":self.characterId,
+            "planetId": self.planetId,
+            "vehicleId": self.vehicleId,
 
-## Generate the ER diagram
-render_er(Base, 'diagram.png')
+        }
